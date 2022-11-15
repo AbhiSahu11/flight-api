16a234bf1dcf173a3e52102857950eda3ed2e3ae
Abhishek Sahu <abhishek.sahu@ing.com>
2022-11-15 22:51:44 +0100

    Unit test for Controller & Integration test fixed

--- src/test/java/nl/abnamro/api/flightsearch/controller/FlightSearchControllerTest.java
+++ src/test/java/nl/abnamro/api/flightsearch/controller/FlightSearchControllerTest.java
@@ -1,49 +1,67 @@
 package nl.abnamro.api.flightsearch.controller;
 
+import nl.abnamro.api.flightsearch.configuration.security.jwt.JwtUtils;
 import nl.abnamro.api.flightsearch.domain.Flight;
 import nl.abnamro.api.flightsearch.service.FlightSearchService;
 import nl.abnamro.api.flightsearch.service.InvalidInputException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest (FlightSearchController.class)
+@WebMvcTest
 class FlightSearchControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private FlightSearchService flightSearchService;
 
+    @MockBean
+    private JwtUtils jwtUtil;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void givenListOfFlight_whenGetAllFlights_thenReturnFlightList() throws Exception{
         // given
         Flight flight= Flight.builder()
@@ -81,6 +99,7 @@ class FlightSearchControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void shouldGiveBadRequest_whenGetAllFlights_ForEmptyOrigin() throws Exception{
         // given
         Flight flight= Flight.builder()
@@ -114,6 +133,7 @@ class FlightSearchControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void shouldGiveBadRequest_whenGetAllFlights_ForEmptyDestination() throws Exception{
         // given
         Flight flight= Flight.builder()
--- src/test/java/nl/abnamro/api/flightsearch/integration/FlightSearchControllerITest.java
+++ src/test/java/nl/abnamro/api/flightsearch/integration/FlightSearchControllerITest.java
@@ -1,24 +1,33 @@
 package nl.abnamro.api.flightsearch.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.abnamro.api.flightsearch.domain.Flight;
+import nl.abnamro.api.flightsearch.payload.request.LoginRequest;
+import nl.abnamro.api.flightsearch.properties.AppDataProperties;
 import nl.abnamro.api.flightsearch.repository.FlightSearchRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,12 +38,22 @@ class FlightSearchControllerITest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private FlightSearchRepository flightSearchRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void givenListOfFlight_whenGetAllFlights_thenReturnFlightList() throws Exception{
         // given
         Flight flight= Flight.builder()
@@ -70,6 +89,7 @@ class FlightSearchControllerITest {
     }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void shouldGiveBadRequest_whenGetAllFlights_ForEmptyOrigin() throws Exception{
         // given
         LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
@@ -90,6 +110,7 @@ class FlightSearchControllerITest {
     }
 
     @Test
+    @WithMockUser(username = "mockuser")
     public void shouldGiveBadRequest_whenGetAllFlights_ForEmptyDestination() throws Exception{
         // given
         LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
